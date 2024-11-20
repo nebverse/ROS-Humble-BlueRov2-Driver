a5abc850ab02f7116906a4dcc0c30aa9f74f5fbf
nebverse <sinhaakshat14@gmail.com>
2024-11-20 11:05:10 +0100

    Added gamepad launch file for gripper

---
+++ install/bluerov2_bringup/share/bluerov2_bringup/launch/gamepad_with_gripper.launch.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    controller_node = Node(
+        package="bluerov2_controller",
+        executable="controller_with_gripper",
+    )
+
+    video_node = Node(
+        package="bluerov2_controller",
+        executable="video",
+    )
+
+    input_node = Node(
+        package="bluerov2_controller",
+        executable="input_controller",
+    )
+
+    depth_node = Node(
+        package="bluerov2_controller",
+        executable="depth_controller",
+    )
+
+    ld.add_action(controller_node)    
+    ld.add_action(depth_node)
+    ld.add_action(video_node)
+    ld.add_action(input_node)
+    return ld
---
+++ src/bluerov2_bringup/launch/gamepad_with_gripper_dry.launch.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    ld = LaunchDescription()
+    pwm_max = 1530
+    pwm_min = 1470
+
+    controller_node = Node(
+        package="bluerov2_controller",
+        executable="controller_with_gripper",
+    )
+
+    video_node = Node(
+        package="bluerov2_controller",
+        executable="video",
+    )
+
+    input_node = Node(
+        package="bluerov2_controller",
+        executable="input_controller",
+        parameters=[
+            {"pwm_max": pwm_max}, 
+            {"pwm_min": pwm_min},                    
+        ],
+    )
+
+    depth_node = Node(
+        package="bluerov2_controller",
+        executable="depth_controller",
+        parameters=[
+            {"pwm_max": pwm_max},
+            {"pwm_min": pwm_min},     
+            {"enable": True},          
+        ],
+    )    
+
+    ld.add_action(controller_node)
+    ld.add_action(video_node)
+    ld.add_action(input_node)
+    ld.add_action(depth_node)
+    return ld
